836880d162d0dc58eedaf9865481395e3a7d4754
Angkit Khadka <khadkaankit85@gmail.com>
2024-07-09 22:13:13 -0400

    server added

---
+++ Public/script4alarm.js
@@ -0,0 +1,84 @@
+//  logic to set the scene for the first time:)
+
+
+function setFirstScene() {
+    let shownProvideLocationAlert
+
+    function displayFirstMap() {
+        navigator.geolocation.getCurrentPosition(gotLocation, deniedLocationPermission)
+    }
+
+    function gotLocation(locationData) {
+        const myLatitue = locationData.coords.latitude
+        const myLongitude = locationData.coords.longitude
+        const accuracy = locationData.coords.accuracy
+        const map = L.map('map').setView([myLatitue, myLongitude], 17);
+
+        function addMarkerAtMyLocation() {
+
+            var circle = L.circle([myLatitue, myLongitude], {
+                color: 'blue',
+                fillColor: 'blue',
+                fillOpacity: 0.5,
+                radius: 10
+            }).addTo(map);
+
+            // console.log(circle)
+            circle._path.classList.add("beeping_circle")
+        }
+
+        addMarkerAtMyLocation()
+
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+    }
+    function deniedLocationPermission(error) {
+        if (error.code === 1 && !shownProvideLocationAlert) {
+            alert("Please provide the access to your location")
+            shownProvideLocationAlert = true
+            displayFirstMap()
+        }
+        if (error.code != 1) {
+            alert("Error fetching user location")
+            console.log(error)
+        }
+    }
+
+    displayFirstMap()
+}
+
+setFirstScene()
+
+//  logic to get the lat and long of user's destination:) quite simple and concise
+// Search function
+function searchLocation() {
+    const searchInput = document.getElementById('location-search').value;
+
+    async function fetchDestination() {
+        const response = await fetch(`/get-my-destination`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ userQuery: searchInput })
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+    fetchDestination()
+
+
+}
+
+// Add event listener to search button
+document.getElementById('search-button').addEventListener('click', searchLocation);
+
+// Function to add alarm (you'll need to implement this)
+function addAlarm(lat, lon) {
+    // Implement your alarm adding logic here
+    console.log(`Adding alarm for location: ${lat}, ${lon}`);
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const axios = require("axios")
+const app = express()
+require("dotenv").config()
+
+// body parser here
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.text())
+
+
+const HOST = "0.0.0.0"
+app.use(express.static(path.join(__dirname, "Public")))
+
+app.get("/", (req, res) => {
+    res.status(201).send(path.join(__dirname, "Public", "index.html"))
+})
+
+app.post("/get-my-destination", (req, res) => {
+    const body = req.body
+    const userRequestedLocation = body?.userQuery
+    console.log(userRequestedLocation)
+
+    // this is the place where i am gonna do that reverse location thing
+
+    async function forwardGeocoding() {
+        const destURL = `https://us1.locationiq.com/v1/search?key=${process.env.LOCATION_API_KEY}&q=${encodeURIComponent(userRequestedLocation)}&format=json&`
+        const response = await axios.get(destURL)
+        console.log(response)
+        res.status(201).send(response)
+
+    }
+    forwardGeocoding()
+
+
+
+})
+
+app.listen(process.env.PORT || 5000, HOST)
